4214659a356297f4fe8b87442ff79f827bdad03d
Jack Boberg <jack.boberg@gmail.com>
2011-11-15 17:55:28 -0500

    initialize asset/templates
    extend MY_Controller classes to begin usefulness
    add default assets and output
    add local version of modernizr lib

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,27 +1,37 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends Public_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   void
+     *
+     * @return  void
+     **/
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Default method
+     *
+     * @access  public
+     * @param   void
+     *
+     * @return  void
+     **/
+    public function index()
 	{
 		$this->template->build('welcome_message');
-	}
-}
+    }
 
+    // --------------------------------------------------------------------
+
+}
 /* End of file welcome.php */
 /* Location: ./application/controllers/welcome.php */
