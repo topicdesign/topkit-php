59f6d81bd4b6b0311d4476729ad3643dcf7376c6
Jack Boberg <jack.boberg@gmail.com>
2011-11-17 17:26:37 -0500

    page router
    add default 404 route
    add pages controller/views/model
    new asset for test page

---
+++ application/controllers/pages.php
@@ -0,0 +1,64 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Pages extends Public_Controller {
+
+    /**
+     * Constructor
+     *
+     * @access  public
+     * @param   void
+     *
+     * @return  void
+     **/
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Attempt to route URI to page record
+     *
+     * @access  public
+     * @param   void
+     *
+     * @return  void
+     **/
+    public function route()
+    {
+        // prepend URI with a '/'
+        $uri = $this->uri->uri_string();
+        if (substr($uri, 0, 1) !== '/')
+        {
+            $uri = '/' . $uri;
+        }
+        // ensure this method is not being access directly
+        if ($uri == $this->uri->ruri_string() OR ! Page::exists($uri))
+        {
+            show_404();
+        }
+        // get (published) page record 
+        if (FALSE) // FPO can('manage', 'page')
+        {
+            $page = Page::find($uri);
+        }
+        else
+        {
+            $page = Page::published($uri);    
+        }
+        if ( ! $page)
+        {
+            show_404();
+        }
+        // output page template
+        $this->template
+            ->title($page->title)
+            ->build('pages/' . $page->view, array('page'=>$page));
+    }
+
+    // --------------------------------------------------------------------
+
+}
+/* End of file pages.php */
+/* Location: ./application/controllers/pages.php */
---
+++ application/views/pages/default.php
@@ -0,0 +1,6 @@
+<section class="page">
+    <header>
+        <h1><?php echo $page->title; ?></h1>
+    </header> 
+    <article><?php echo $page->content; ?></article>
+</section>
