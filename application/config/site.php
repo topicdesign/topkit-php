2b99363e133b6bc8e8d5423bfce1339834983047
Seth Baur <sethbaur@gmail.com>
2012-01-10 11:18:10 -0500

    readding config files

---
+++ application/config/site.php
@@ -0,0 +1,47 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------------
+| Site Config
+| -------------------------------------------------------------------------
+*/
+
+/**
+ * Google Analaytics Account ID
+ *
+ * @var string
+ **/
+$config['google_analytics_id'] = NULL;
+
+/**
+ * Default date format
+ *
+ * @var string
+ **/
+$config['site_date_format'] = 'F jS Y \a\t g:iA';
+
+/**
+ * Default date timezone
+ *
+ * @var string
+ **/
+$config['site_timezone'] = 'America/New_York';
+
+/**
+ * Email address for site developer
+ *
+ * @var string
+ **/
+$config['developer_email'] = '';
+
+// --------------------------------------------------------------------
+// Do not edit below this line, managed by CMS
+// --------------------------------------------------------------------
+$config['site_title'] = 'Topkit';
+$config['site_description'] = '';
+$config['site_owner_title'] = '';
+$config['site_owner_url'] = '';
+$config['site_email'] = '';
+$config['site_copy_year'] = '';
+
+/* End of file site.php */
+/* Location: ./config/site.php */
