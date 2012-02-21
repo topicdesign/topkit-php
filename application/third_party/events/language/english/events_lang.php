4a3ecfe99484b389c8d9cde4419d40d83ea5efb6
Jack Boberg <jack.boberg@gmail.com>
2012-02-21 16:16:00 -0500

    remove articles/events from base repo to be added on a per project basis

--- application/third_party/events/language/english/events_lang.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$lang['event_calendar_title']       = "Calendar";
-$lang['events_not_found'] = 'No events on file.';
-
-/* End of file events_lang.php */
-/* Location: ./third_party/language/english/events_lang.php */
