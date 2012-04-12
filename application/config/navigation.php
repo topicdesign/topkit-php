ca6a673be098da7bb4c562565b1d9217d295e768
Seth Baur <sethbaur@gmail.com>
2012-04-12 13:56:54 -0400

    replacing admin sidebar nav with nav helper

--- application/config/navigation.php
+++ application/config/navigation.php
@@ -25,6 +25,28 @@ $config['nav_template']['default'] = array(
         ),
     ),
 );
+$config['nav_template']['admin_sidebar'] = array(
+    'wrapper'   => array(
+        'tag'           => 'nav',
+        'class'         => '',
+        'attributes'    => array(
+        ),
+    ),
+    'list'      => array(
+        'tag'           => 'ul',
+        'class'         => 'nav nav-tabs nav-stacked',
+        'attributes'    => array(
+            'type'  => 'toolbar',
+            'id'    => 'modules',
+        ),
+    ),
+    'list_item' => array(
+        'tag'           => 'li',
+        'class'         => '',
+        'attributes'    => array(
+        ),
+    ),
+);
 
 /* EXAMPLE
 $config['anchors']['main'] = array(
@@ -95,6 +117,77 @@ $config['anchors']['footer'] = array(
         ),
         'prepend' => '',
         'append'  => '',
+    ),
+);
+$config['anchors']['admin_sidebar'] = array(
+    array(
+        'href'  => '/admin',
+        'label' => '<i class="icon-home"></i>Dashboard',
+        'attr'  => array(
+            'title' => 'Dashboard',
+        ),
+        'prepend' => '',
+        'append'  => '',
+    ),
+    array(
+        'href'  => '/admin/settings',
+        'label' => 'Settings',
+        'attr'  => array(
+            'title' => 'Settings',
+        ),
+        'prepend' => '',
+        'append'  => '',
+        'permissions' => array(
+            //'setting' => 'update',
+        ),
+    ),
+    array(
+        'href'  => '/admin/events',
+        'label' => 'Events',
+        'attr'  => array(
+            'title' => 'Events',
+        ),
+        'prepend' => '',
+        'append'  => '',
+        'permissions' => array(
+            //'event' => 'update',
+        ),
+    ),
+    array(
+        'href'  => '/admin/pages',
+        'label' => '<i class="icon-file"></i>Pages',
+        'attr'  => array(
+            'title' => 'Pages',
+        ),
+        'prepend' => '',
+        'append'  => '',
+        'permissions' => array(
+            //'page' => 'update',
+        ),
+    ),
+    array(
+        'href'  => '/admin/members',
+        'label' => 'Members',
+        'attr'  => array(
+            'title' => 'Members',
+        ),
+        'prepend' => '',
+        'append'  => '',
+        'permissions' => array(
+            //'user' => 'manage',
+        ),
+    ),
+    array(
+        'href'  => '/admin/users',
+        'label' => 'Users',
+        'attr'  => array(
+            'title' => 'Users',
+        ),
+        'prepend' => '',
+        'append'  => '',
+        'permissions' => array(
+            //'user' => 'manage',
+        ),
     ),
 );
 
--- application/views/_partials/admin_sidebar.php
+++ application/views/_partials/admin_sidebar.php
@@ -1,58 +1,7 @@
 <aside id="sidebar" class="span2">
 <?php if ( ! logged_in()): ?>
     <p>Authenticate to access the administration tools.</p>
-<?php else: ?>
-	<ul id="modules" type="toolbar" class="nav nav-tabs nav-stacked">
-	    <li><a href="<?php echo site_url('admin'); ?>"><i class="icon-home"></i>Dashboard</a></li>
-        <?php
-        $links = array(
-            array(
-                'label'         => 'Settings',
-                'url'           => site_url('admin/settings'),
-                'require'       => array(
-                    'setting'   => 'update'
-                ),
-                'data'          => array(
-                    'inactive'      => TRUE
-                )
-            ),
-            array(
-                'label'         => 'Events',
-                'url'           => site_url('admin/events'),
-                'require'       => array(
-                    'event'   => 'update'
-                ),
-                'data'          => array(
-                    'inactive'      => TRUE
-                ),
-            ),
-            array(
-                'label'         => '<i class="icon-file"></i>Pages',
-                'url'           => site_url('admin/pages'),
-                'require'       => array(
-                    'page'      => 'update'
-                ),
-                'data'          => array(
-                    'inactive'      => TRUE
-                )
-            ),
-            array(
-                'label'         => 'Members',
-                'url'           => site_url('admin/members'),
-                'require'       => array(
-                    'user'      => 'manage'
-                ),
-            ),
-            array(
-                'label'         => 'Users',
-                'url'           => site_url('admin/users'),
-                'require'       => array(
-                    'user'      => 'manage'
-                ),
-            ),
-        );
-        echo get_module_menu($links);
-        ?>
-	</ul>
-<?php endif; ?>
+<?php else: 
+        echo get_nav('admin_sidebar', TRUE, 'admin_sidebar');
+      endif; ?>
 </aside>
