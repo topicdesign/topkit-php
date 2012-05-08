34b53c717b1941b0006a60cff0b56213ecc8f4bf
Seth Baur <sethbaur@gmail.com>
2012-05-08 13:23:17 -0400

    adding form validation to login

--- application/views/users/forgot.php
+++ application/views/users/forgot.php
@@ -3,7 +3,7 @@
     <form action="<?php echo site_url('login/forgot'); ?>" method="POST" class="well">
         <fieldset>
             <label for="identity" class="text">Identity</label>
-                <input type="text" id="identity" name="identity" placeholder="user@example.com" class="text" <?php echo set_value('identity'); ?> />
+                <input type="text" id="identity" name="identity" placeholder="user@example.com" class="text" value="<?php echo set_value('identity'); ?>" />
                 <p class="help-inline">Your identity can be your <strong>username</strong> or <strong>email address</strong>.</p>
         </fieldset>
         <fieldset class="form-actions">
